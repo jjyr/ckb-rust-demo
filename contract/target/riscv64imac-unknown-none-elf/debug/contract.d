9f4cc46e41c7fc031a3f0e075b3bf3bd15c8d33a
jjy <jjyruby@gmail.com>
2019-12-30 13:42:00 +0800

    remove target

--- contract/target/riscv64imac-unknown-none-elf/debug/contract.d
+++
@@ -1,1 +0,0 @@
-/Users/jiangjinyang/workspace/ckb-rust-demo/contract/target/riscv64imac-unknown-none-elf/debug/contract: /Users/jiangjinyang/workspace/ckb-rust-demo/contract/src/main.rs
--- contract/target/riscv64imac-unknown-none-elf/debug/deps/contract-57e4f791d847027d.d
+++
@@ -1,5 +0,0 @@
-/Users/jiangjinyang/workspace/ckb-rust-demo/contract/target/riscv64imac-unknown-none-elf/debug/deps/contract-57e4f791d847027d: src/main.rs
-
-/Users/jiangjinyang/workspace/ckb-rust-demo/contract/target/riscv64imac-unknown-none-elf/debug/deps/contract-57e4f791d847027d.d: src/main.rs
-
-src/main.rs:
