861c30b6cd3580bf518e809a7751f765279c8e9f
jjy <jjyruby@gmail.com>
2020-01-05 23:20:03 +0800

    rewrite contract with ckb-contract-std macro

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1,52 +1,17 @@
 #![no_std]
 #![no_main]
-#![feature(start)]
-#![feature(asm)]
 #![feature(lang_items)]
 #![feature(alloc_error_handler)]
-#![feature(const_raw_ptr_to_usize_cast)]
+#![feature(panic_info_message)]
 
-use buddy_alloc::{NonThreadsafeAlloc, REQUIRED_SPACE};
-use ckb_contract_std::{syscalls::{exit, }, debug};
+use ckb_contract_std::{debug, setup};
 use alloc::vec;
 
-extern crate alloc;
-
-pub static mut HEAP_VECTOR:[u8;REQUIRED_SPACE] = [0u8;REQUIRED_SPACE];
-
-#[global_allocator]
-static ALLOC: NonThreadsafeAlloc = unsafe{NonThreadsafeAlloc::new(HEAP_VECTOR.as_ptr())};
-
-#[alloc_error_handler]
-fn oom_handler(_: alloc::alloc::Layout) -> ! {
-    exit(-128i8);
-    loop{}
-}
-
 #[no_mangle]
-pub extern "C" fn main() -> i8 {
+pub fn main() -> i8 {
     let v = vec![0u8;42];
-    debug!("{:?}", v);
+    debug!("{:?}", v.len());
     0
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    exit(main());
-    loop{}
-}
-
-#[panic_handler]
-fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
-    debug!("{:?}", panic_info);
-    exit(-100i8);
-    loop {}
-}
-
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
-
-#[no_mangle]
-pub extern fn abort() {
-    panic!("abort!");
-}
+setup!(main);
