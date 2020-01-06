23a1ad80322c9ef61d60fb1f23a1051be45159e0
jjy <jjyruby@gmail.com>
2020-01-06 11:58:52 +0800

    define entry point without linker

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1,25 +1,22 @@
 #![no_std]
 #![no_main]
-#![feature(start)]
 #![feature(asm)]
 #![feature(lang_items)]
 
 #[no_mangle]
-#[start]
-pub fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    exit(0);
-    // just ignore this return value
-    0
+pub fn _start() -> ! {
+    exit(0)
 }
 
 /// Exit syscall
 /// https://github.com/nervosnetwork/rfcs/blob/master/rfcs/0009-vm-syscalls/0009-vm-syscalls.md
-pub fn exit(_code: i8) {
+pub fn exit(_code: i8) -> ! {
     unsafe {
         // a0 is _code
         asm!("li a7, 93");
         asm!("ecall");
     }
+    loop {}
 }
 
 #[panic_handler]
@@ -29,3 +26,8 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
+
+#[no_mangle]
+pub fn abort() -> ! {
+    panic!("abort!")
+}
